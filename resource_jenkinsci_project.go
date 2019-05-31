e4ff25aafe732dd1d8831faeb1b196fc1c748ca5
DanielMabbett <526473+gb96@users.noreply.github.com>
2019-05-31 10:33:42 +0100

    add description and disabled params for project

--- resource_jenkinsci_project.go
+++ resource_jenkinsci_project.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 
+	"github.com/beevik/etree"
 	jenkins "github.com/bndr/gojenkins"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
 )
 
 func resourceProject() *schema.Resource {
@@ -30,6 +32,17 @@ func resourceProject() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
+			"disabled": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "false",
+				ValidateFunc: validation.StringInSlice([]string{"false", "true"}, true),
+			},
+			"assigned_node": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -37,29 +50,62 @@ func resourceProject() *schema.Resource {
 func resourceProjectCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*jenkins.Jenkins)
 	name := d.Get("name").(string)
-	configString := `<?xml version='1.0' encoding='UTF-8'?>
-        <project>
-          <actions/>
-          <description></description>
-          <keepDependencies>false</keepDependencies>
-          <properties/>
-          <scm class="hudson.scm.NullSCM"/>
-          <canRoam>true</canRoam>
-          <disabled>false</disabled>
-          <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
-          <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
-          <triggers class="vector"/>
-          <concurrentBuild>false</concurrentBuild>
-          <builders/>
-          <publishers/>
-          <buildWrappers/>
-        </project>`
+	desc := d.Get("description").(string)
+	assNode := d.Get("assigned_node").(string)
+	disab := d.Get("disabled").(string)
+
+	doc := etree.NewDocument()
+	doc.CreateProcInst("xml", `version="1.0" encoding="UTF-8"`)
+	project := doc.CreateElement("project")
+
+	project.CreateElement("actions")
+	description := project.CreateElement("description")
+	description.CreateText(desc)
+	keepDependencies := project.CreateElement("keepDependencies")
+	keepDependencies.CreateText("false")
+
+	// Param Definitions Section
+	// parameterDefinitions := project.CreateElement("parameterDefinitions")
+	// parameterDefinitions.CreateElement
+
+	project.CreateElement("properties")
+	scmclass := project.CreateElement("scm")
+	scmclass.CreateAttr("class", "hudson.scm.NullSCM")
+
+	// Assigned Node to the project
+	assignedNode := project.CreateElement("assignedNode")
+	assignedNode.CreateText(assNode)
+
+	// Can roam?
+	canRoam := project.CreateElement("canRoam")
+	canRoam.CreateText("true")
+
+	// Define if disabled or not
+	disabled := project.CreateElement("disabled")
+	disabled.CreateText(disab)
+
+	blockBuildWhenDownstreamBuilding := project.CreateElement("blockBuildWhenDownstreamBuilding")
+	blockBuildWhenDownstreamBuilding.CreateText("false")
+	blockBuildWhenUpstreamBuilding := project.CreateElement("blockBuildWhenUpstreamBuilding")
+	blockBuildWhenUpstreamBuilding.CreateText("false")
+	triggers := project.CreateElement("triggers")
+	triggers.CreateAttr("class", "vector")
+	concurrentBuild := project.CreateElement("concurrentBuild")
+	concurrentBuild.CreateText("false")
+	project.CreateElement("builders")
+	project.CreateElement("publishers")
+	project.CreateElement("buildWrappers")
+
+	str, err := doc.WriteToString()
+	if err != nil {
+		panic(err)
+	}
 
 	if _, ok := d.GetOk("folder"); ok {
 		folder := d.Get("folder").(string)
-		client.CreateJobInFolder(configString, name, folder)
+		client.CreateJobInFolder(str, name, folder)
 	} else {
-		_, err := client.CreateJob(configString, name)
+		_, err := client.CreateJob(str, name)
 		if err != nil {
 			panic(err)
 		}
--- test.tf
+++ test.tf
@@ -1,15 +1,22 @@
 provider "jenkinsci" {
-  jenkins_endpoint         = "http://localhost:8080"
-  jenkins_admin_username   = "admin"
-  jenkins_admin_password   = "547b55dbeb9240d5b345a772d8905325"
+  jenkins_endpoint       = "http://localhost:8080"
+  jenkins_admin_username = "admin"
+  jenkins_admin_password = "547b55dbeb9240d5b345a772d8905325"
 }
 
 resource "jenkinsci_project" "test" {
   name = "testproj"
 }
 
+resource "jenkinsci_project" "test2" {
+  name          = "testproj2"
+  description   = "my test project - version 2"
+  disabled      = "true"
+  assigned_node = "terraform-pod"
+}
+
 resource "jenkinsci_project" "test-in-folder" {
-  name = "testprojinfolder"
+  name   = "testprojinfolder"
   folder = "${jenkinsci_folder.test.name}"
 }
 
